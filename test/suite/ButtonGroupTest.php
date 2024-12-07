0712ed61aad9f8af2f250d6b2f5286f40fcba0ef
eylemugurel <eylemugurel@hotmail.com>
2024-12-07 18:24:00 +0100

    Update tests

--- test/suite/ButtonGroupTest.php
+++ test/suite/ButtonGroupTest.php
@@ -12,46 +12,34 @@ class ButtonGroupTest extends TestCase
     {
         $buttonGroup = new ButtonGroup();
         $this->assertEquals(
-            '<div class="btn-group" role="group" aria-label=""></div>',
-            $buttonGroup->Render()
+            $buttonGroup->Render(),
+            '<div class="btn-group" role="group" aria-label=""></div>'
+        );
+    }
+
+    function testRenderVertical()
+    {
+        $buttonGroup = new ButtonGroup(['class' => 'btn-group-vertical']);
+        $this->assertEquals(
+            $buttonGroup->Render(),
+            '<div class="btn-group-vertical" role="group" aria-label=""></div>'
         );
     }
 
     function testRenderWithButtons()
     {
-        $button1 = new Button(['class' => 'btn-warning'], 'Button 1');
-        $button2 = new Button(['class' => 'btn-danger'], 'Button 2');
-        $buttonGroup = new ButtonGroup(null, [$button1, $button2]);
+        $buttonGroup = new ButtonGroup(['aria-label'=>'Basic button group'], [
+          new Button(null, 'Left'),
+          new Button(null, 'Middle'),
+          new Button(null, 'Right')
+        ]);
         $this->assertEquals(
-            '<div class="btn-group" role="group" aria-label="">'
-            . '<button type="button" class="btn btn-warning">Button 1</button>'
-            . '<button type="button" class="btn btn-danger">Button 2</button>'
-            . '</div>',
-            $buttonGroup->Render()
-        );
-    }
-
-    function testCustomAttributes()
-    {
-        $button1 = new Button(null, 'Button 1');
-        $buttonGroup = new ButtonGroup(
-            ['id' => 'testGroup', 'class' => 'btn-group-lg', 'aria-label' => 'Test Group'],
-            [$button1]
-        );
-        $this->assertEquals(
-            '<div class="btn-group-lg btn-group" role="group" aria-label="Test Group" id="testGroup">'
-            . '<button type="button" class="btn btn-primary">Button 1</button>'
-            . '</div>',
-            $buttonGroup->Render()
-        );
-    }
-
-    function testMutuallyExclusiveClasses()
-    {
-        $buttonGroup = new ButtonGroup(['class' => 'btn-group-vertical']);
-        $this->assertEquals(
-            '<div class="btn-group-vertical" role="group" aria-label=""></div>',
-            $buttonGroup->Render()
+            $buttonGroup->Render(),
+            '<div class="btn-group" role="group" aria-label="Basic button group">'
+          .   '<button type="button" class="btn btn-primary">Left</button>'
+          .   '<button type="button" class="btn btn-primary">Middle</button>'
+          .   '<button type="button" class="btn btn-primary">Right</button>'
+          . '</div>'
         );
     }
 }
--- test/suite/ButtonTest.php
+++ test/suite/ButtonTest.php
@@ -11,19 +11,106 @@ class ButtonTest extends TestCase
     {
         $button = new Button();
         $this->assertEquals(
-            '<button type="button" class="btn btn-primary"></button>',
-            $button->Render()
+            $button->Render(),
+            '<button type="button" class="btn btn-primary"></button>'
         );
     }
 
-    function testRenderWithCustomAttributes()
+    function testRenderVariants()
     {
-        $button = new Button([
-            'id' => 'testButton', 'class' => 'btn-danger'
-        ], 'Click Me');
+        $button = new Button(['class' => 'btn-secondary'], 'Secondary');
         $this->assertEquals(
-            '<button type="button" class="btn btn-danger" id="testButton">Click Me</button>',
-            $button->Render()
+            $button->Render(),
+            '<button type="button" class="btn btn-secondary">Secondary</button>'
+        );
+        $button = new Button(['class' => 'btn-success'], 'Success');
+        $this->assertEquals(
+            $button->Render(),
+            '<button type="button" class="btn btn-success">Success</button>'
+        );
+        $button = new Button(['class' => 'btn-info'], 'Info');
+        $this->assertEquals(
+            $button->Render(),
+            '<button type="button" class="btn btn-info">Info</button>'
+        );
+        $button = new Button(['class' => 'btn-warning'], 'Warning');
+        $this->assertEquals(
+            $button->Render(),
+            '<button type="button" class="btn btn-warning">Warning</button>'
+        );
+        $button = new Button(['class' => 'btn-danger'], 'Danger');
+        $this->assertEquals(
+            $button->Render(),
+            '<button type="button" class="btn btn-danger">Danger</button>'
+        );
+        $button = new Button(['class' => 'btn-light'], 'Light');
+        $this->assertEquals(
+            $button->Render(),
+            '<button type="button" class="btn btn-light">Light</button>'
+        );
+        $button = new Button(['class' => 'btn-dark'], 'Dark');
+        $this->assertEquals(
+            $button->Render(),
+            '<button type="button" class="btn btn-dark">Dark</button>'
+        );
+        $button = new Button(['class' => 'btn-link'], 'Link');
+        $this->assertEquals(
+            $button->Render(),
+            '<button type="button" class="btn btn-link">Link</button>'
+        );
+        $button = new Button(['class' => 'btn-outline-primary'], 'Primary');
+        $this->assertEquals(
+            $button->Render(),
+            '<button type="button" class="btn btn-outline-primary">Primary</button>'
+        );
+        $button = new Button(['class' => 'btn-outline-secondary'], 'Secondary');
+        $this->assertEquals(
+            $button->Render(),
+            '<button type="button" class="btn btn-outline-secondary">Secondary</button>'
+        );
+        $button = new Button(['class' => 'btn-outline-success'], 'Success');
+        $this->assertEquals(
+            $button->Render(),
+            '<button type="button" class="btn btn-outline-success">Success</button>'
+        );
+        $button = new Button(['class' => 'btn-outline-info'], 'Info');
+        $this->assertEquals(
+            $button->Render(),
+            '<button type="button" class="btn btn-outline-info">Info</button>'
+        );
+        $button = new Button(['class' => 'btn-outline-warning'], 'Warning');
+        $this->assertEquals(
+            $button->Render(),
+            '<button type="button" class="btn btn-outline-warning">Warning</button>'
+        );
+        $button = new Button(['class' => 'btn-outline-danger'], 'Danger');
+        $this->assertEquals(
+            $button->Render(),
+            '<button type="button" class="btn btn-outline-danger">Danger</button>'
+        );
+        $button = new Button(['class' => 'btn-outline-light'], 'Light');
+        $this->assertEquals(
+            $button->Render(),
+            '<button type="button" class="btn btn-outline-light">Light</button>'
+        );
+        $button = new Button(['class' => 'btn-outline-dark'], 'Dark');
+        $this->assertEquals(
+            $button->Render(),
+            '<button type="button" class="btn btn-outline-dark">Dark</button>'
+        );
+    }
+
+    function testRenderSizes()
+    {
+        $button = new Button(['class'=>'btn-sm'], 'Small button');
+        $this->assertEquals(
+            $button->Render(),
+            '<button type="button" class="btn-sm btn btn-primary">Small button</button>'
+        );
+        $button = new Button(['class'=>'btn-lg'], 'Large button');
+        $this->assertEquals(
+            $button->Render(),
+            '<button type="button" class="btn-lg btn btn-primary">Large button</button>'
         );
     }
 }
